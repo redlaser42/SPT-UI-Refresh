489014c2d9fbfa3d1eae92cfc278ea5903baaecb
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2025-08-30 13:02:07 -0400

    publish .dll

--- Patches.cs
+++ Patches.cs
@@ -1,17 +1,16 @@
 ﻿using BepInEx.Logging;
 using EFT;
-using EFT.EnvironmentEffect;
 using EFT.HealthSystem;
 using EFT.InventoryLogic;
 using EFT.UI;
 using EFT.UI.Matchmaker;
 using HarmonyLib;
-using JsonType;
 using SPT.Reflection.Patching;
 using System;
 using System.Reflection;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace UIRefresh.Patches
@@ -28,24 +27,31 @@ namespace UIRefresh.Patches
         [PatchPostfix]
         static void Postfix(MatchMakerSideSelectionScreen __instance)
         {
+            // Deactivates Logo and texts.
             __instance.transform.Find("CaptionsHolder").gameObject.SetActive(false);
             __instance.transform.Find("Logo").gameObject.SetActive(false);
             __instance.transform.Find("Description").gameObject.SetActive(false);
 
+            // Adjust PMC model
             var PMC = __instance.transform.Find("PMCs");
-                PMC.GetComponent<RectTransform>().anchoredPosition = new Vector2(50, -200f);
+                PMC.GetComponent<RectTransform>().anchoredPosition = new Vector2(50, -190f);
                 PMC.transform.Find("PMCPlayerMV").GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 150);
-                PMC.transform.Find("PMCPlayerMV").Find("PlayerMVObject").Find("Camera_matchmaker").GetComponent<Camera>().fieldOfView = 28;
-                PMC.transform.Find("PMCPlayerMV").GetComponent<RectTransform>().sizeDelta = new Vector2(800, 0);
+                PMC.transform.Find("PMCPlayerMV").Find("PlayerMVObject").Find("Camera_matchmaker").GetComponent<Camera>().fieldOfView = 20;
+            PMC.transform.Find("PMCPlayerMV").Find("PlayerMVObject").Find("Camera_matchmaker").GetComponent<Transform>().localPosition = new Vector3(0.62f, -0.4f, 0.96f);
+            PMC.transform.Find("PMCPlayerMV").GetComponent<RectTransform>().sizeDelta = new Vector2(1000, 0);
 
+            //Adjust SCAV model
             var SCAV = __instance.transform.Find("Savage");
-                SCAV.GetComponent<RectTransform>().anchoredPosition = new Vector2(-50, -190f);
-                SCAV.transform.Find("ScavPlayerMV").Find("PlayerMVObject").Find("Camera_matchmaker").GetComponent<Camera>().fieldOfView = 28;
-                SCAV.transform.Find("ScavPlayerMV").GetComponent<RectTransform>().sizeDelta = new Vector2(800, 0);
-                SCAV.transform.Find("ScavPlayerMV").GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 150);
+            SCAV.GetComponent<RectTransform>().anchoredPosition = new Vector2(-50, -190f);
+            SCAV.transform.Find("ScavPlayerMV").Find("PlayerMVObject").Find("Camera_matchmaker").GetComponent<Camera>().fieldOfView = 20;
+            SCAV.transform.Find("ScavPlayerMV").GetComponent<RectTransform>().sizeDelta = new Vector2(1000, 0);
+            SCAV.transform.Find("ScavPlayerMV").GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 150);
+            SCAV.transform.Find("ScavPlayerMV").Find("PlayerMVObject").Find("Camera_matchmaker").GetComponent<Transform>().localPosition = new Vector3(-1.71f, -0.39f, 1.28f);
+
         }
     }
 
+    // Level Select Map Menu
     internal class LocationSelectionPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
@@ -56,14 +62,20 @@ namespace UIRefresh.Patches
         [PatchPostfix]
         static void Postfix(MatchMakerSelectionLocationScreen __instance)
         {
+            //Deactivates top text and stripped background of condidions panel.
             __instance.transform.Find("CaptionsHolder").gameObject.SetActive(false);
             __instance.transform.Find("Conditions Panel").Find("Tiles").gameObject.SetActive(false);
+
+            // Centers Map.
             __instance.transform.Find("Content").GetComponent<RectTransform>().anchoredPosition = new Vector2(200, 0);
+
+            //Increases location name and icon. 
             __instance.transform.Find("Content").Find("Location Info Panel").GetComponent<RectTransform>().anchoredPosition = new Vector2(100, -160);
             __instance.transform.Find("Content").Find("Location Info Panel").Find("Banner").GetComponent <RectTransform>().localScale = new Vector3(1.76f, 1.76f , 1);
         }
     }
-    //Raid Settings Screen
+
+    //Offline Raid Settings Menu
     internal class RaidSettingsScreenPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
@@ -74,12 +86,14 @@ namespace UIRefresh.Patches
         [PatchPostfix]
         static void Postfix(MatchmakerOfflineRaidScreen __instance)
         {
+            // Deactivates top text and spacers.
             __instance.transform.Find("Content").Find("Description").gameObject.SetActive(false);
             __instance.transform.Find("Content").Find("Space (1)").gameObject.SetActive(false);
             __instance.transform.Find("Content").Find("Space (2)").gameObject.SetActive(false);
         }
     }
-    //Insurance Screen
+
+    //Insurance Menu
     internal class InsuranceScreenPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
@@ -100,7 +114,6 @@ namespace UIRefresh.Patches
         }
     }
 
-
     //Accept Location Menu
     internal class AcceptLocationPatch : ModulePatch
     {
@@ -112,9 +125,11 @@ namespace UIRefresh.Patches
         [PatchPostfix]
         static void Postfix(MatchMakerAcceptScreen __instance)
         {
+            // Deactiavtes text on top and time/weather pannel.
             __instance.transform.Find("CaptionsHolder").gameObject.SetActive(false);
             __instance.transform.Find("Conditions Panel").gameObject.SetActive(false);
 
+            // Adjust player model
             var previewPanel = __instance.transform.Find("PreviewsPanel");
                 previewPanel.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(20f, 155f);
                 previewPanel.transform.Find("CurrentPlayerModelView").Find("PlayerMVObject").Find("Camera_acceptScreen").GetComponent<Camera>().fieldOfView = 12;
@@ -126,8 +141,7 @@ namespace UIRefresh.Patches
         }
     }
 
-
-    //Pre-Raid Loading Screen
+    //Raid Loading Screen
     internal class TimeHasComePatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
@@ -138,30 +152,39 @@ namespace UIRefresh.Patches
         [PatchPostfix]
         static void Postfix(MatchmakerBannersPanel ____bannersPanel, PlayerModelView ____playerModelView, MatchmakerTimeHasCome __instance)
         {
+            //Deactivate banner, logo, tips, and loading spinner.
             ____bannersPanel.gameObject.SetActive(false);
             __instance.transform.Find("CaptionsHolder").gameObject.SetActive(false);
             __instance.transform.Find("Logo").gameObject.SetActive(false);
             __instance.transform.Find("Loader").gameObject.SetActive(false);
 
+            //Other Players in Raid list - move
             __instance.transform.Find("PartyInfoPanel").GetComponent<RectTransform>().localPosition = new Vector2(-922, 550);
 
+            //Map Name - move, increase font, set background to match.
             var locationName = __instance.transform.Find("Location Name Panel");
                 locationName.gameObject.GetComponent<RectTransform>().localPosition = new Vector2(-880,950);
                 locationName.gameObject.transform.Find("Name").GetComponent<CustomTextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
                 locationName.gameObject.transform.Find("Name").GetComponent<CustomTextMeshProUGUI>().fontSize = 130;
                 locationName.gameObject.transform.Find("Background").GetComponent<RectTransform>().sizeDelta = new Vector2(450, 150);
-            locationName.gameObject.transform.Find("Background").GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+                locationName.gameObject.transform.Find("Background").GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
 
+            //Loading Status Text - move and align left
             var deployCaption = __instance.transform.Find("Deploying Caption");
                 deployCaption.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-746, 17);
                 deployCaption.gameObject.GetComponent<CustomTextMeshProUGUI>().alignment = TextAlignmentOptions.Left;
 
-            //Start and Back Button
+            //Start and Back Button - move
                 __instance.transform.Find("Back Button Panel").gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-840, 55);
 
+            //Player Model - move and resize.
             var playerModel = __instance.transform.Find("PlayerModelView");
                 playerModel.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(800f, 0f);
-                playerModel.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(800f, 0f);
+                playerModel.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(1000, 0f);
+                playerModel.gameObject.GetComponent<AspectRatioFitter>().aspectRatio = 2.7f;
+
+
+            //Player Model Lights. Deactiavate all but Main and adjust.
             playerModel.Find("PlayerModelViewObject").Find("Camera_timehascome0").GetComponent<Camera>().fieldOfView = 26;
                 var lights = playerModel.Find("PlayerModelViewObject").Find("Lights");
                 for (int i = 0; i < lights.childCount; i++)
@@ -179,6 +202,7 @@ namespace UIRefresh.Patches
                     mainLight.GetComponent<Light>().intensity = 6f;
                     mainLight.GetComponent<Light>().color = new Color(1, 0.86f, 0.74f, 1);
 
+            //Remove Bottom Task Bar except for Settings button.
             PreloaderUI preloaderUI = MonoBehaviourSingleton<PreloaderUI>.Instance;
 
             if (preloaderUI == null)
@@ -195,11 +219,10 @@ namespace UIRefresh.Patches
                 Transform child = taskbarTabs.GetChild(i);
                 child.gameObject.SetActive(child.name == "Settings");
             }
-            //preloaderUI.SetMenuTaskBarVisibility(false);
         }
     }
 
-
+    // "Deploying In.." Screen
     internal class FinalCountdownPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
@@ -214,7 +237,7 @@ namespace UIRefresh.Patches
         }
     }
 
-
+    // Add Clock to Inventory Screen
     internal class ClockPatch : ModulePatch
     {
 
