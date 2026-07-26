e629d570e11c844a58f61029626723525e434fdf
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2026-07-26 11:49:39 -0400

    Fixed Ammo Icon Opacity

--- Patches/AmmoPanel_ShowPatch.cs
+++ Patches/AmmoPanel_ShowPatch.cs
@@ -10,6 +10,7 @@ namespace UIRefresh.Patches
     internal class AmmoPannel_ShowPatch : ModulePatch
     {
         public static GameObject? AmmoCountPanelObject = null;
+        public static Image? AmmoIcon = null;
         public static TextMeshProUGUI[]? AmmoPanelTextMeshProUGUI = null;
 
 
@@ -23,6 +24,7 @@ namespace UIRefresh.Patches
         {
             AmmoPanelTextMeshProUGUI = __instance.gameObject.GetComponentsInChildren<TextMeshProUGUI>();
             var AmmoPanelBG = __instance.gameObject.GetComponent<Image>();
+            AmmoIcon = __instance.transform.Find("/Ammo/Details/AmmoImage/").GetComponent<Image>();
             AmmoPanelBG.ChangeImageAlpha(0);
 
             AmmoPanelUpdate();
@@ -36,7 +38,13 @@ namespace UIRefresh.Patches
                 {
                     ammoText.alpha = Plugin.Instance.UIRefreshConfig.AmmoPanelAlpha.Value;
                 }
-            }
+
+                if(AmmoIcon != null)
+                {
+                    AmmoIcon.color = new Color(1, 1, 1, Plugin.Instance.UIRefreshConfig.AmmoPanelAlpha.Value);
+
+                }
+            }       
         }
     }
 }
