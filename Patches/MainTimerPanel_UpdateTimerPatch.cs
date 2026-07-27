bd929c8fd5b18da8097610299e15af0f7298ad9c
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2026-07-27 09:01:52 -0400

    Created Timer Panel Patches

---
+++ Patches/MainTimerPanel_UpdateTimerPatch.cs
@@ -0,0 +1,43 @@
+﻿using EFT.UI.BattleTimer;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace UIRefresh.Patches
+{
+    internal class MainTimerPanel_UpdatePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(MainTimerPanel).GetMethod(nameof(MainTimerPanel.UpdateTimer));
+        }
+
+        [PatchPrefix]
+        static bool Prefix(MainTimerPanel __instance, Color ____warningColor, TimeSpan ___TimeSpan)
+        {
+            if (Plugin.Instance.UIRefreshConfig.HideRaidTimerWarning.Value)
+            {
+                TimerPanelUpdateReversePatch.UpdateTimer(__instance);
+
+                __instance.TimerText.color = new Color(0,0,1);
+
+                bool lowOnTime = ___TimeSpan.TotalSeconds < 600.0;
+
+                if (lowOnTime && !__instance.ForcePull)
+                {
+                    __instance.DisplayTimer();
+                    if (__instance.TimerText.color != ____warningColor)
+                    {
+                        __instance.TimerText.color = ____warningColor;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Patches/TimerPanelUpdateReversePatch.cs
@@ -0,0 +1,17 @@
+﻿using EFT.UI.BattleTimer;
+using HarmonyLib;
+
+namespace UIRefresh.Patches
+{
+    internal class TimerPanelUpdateReversePatch
+    {
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(TimerPanel), "UpdateTimer")]
+
+        public static void UpdateTimer(TimerPanel instance)
+        {
+
+        }
+
+    }
+}
